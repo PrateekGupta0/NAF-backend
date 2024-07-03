d664bb8b6993e0bc88f877ee0b541432f0bb6eaa
PrateekGupta0 <20UCS144@lnmiit.ac.in>
2024-07-03 21:17:02 +0530

    mysql stepup updated

--- src/main/java/com/example/NAF/model/User.java
+++ src/main/java/com/example/NAF/model/User.java
@@ -1,4 +1,29 @@
 package com.example.NAF.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="USER")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="USER_IDENTIFIER")
+    private Integer userIdentifier;
+
+    @Column(name="firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "active_Indicator")
+    private String activeIndicator;
 }
